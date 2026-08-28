685f8e6aa3c6977a7c32a4ee221caaed3b2e4d87
Farukh44 <vokhma44@yandex.ru>
2026-08-28 15:36:40 +0300

    Исправить импорт Content-Type в fleet-клиенте

--- agent/src/main/kotlin/ru/ruznak/netscan/fleet/FleetClient.kt
+++ agent/src/main/kotlin/ru/ruznak/netscan/fleet/FleetClient.kt
@@ -5,10 +5,10 @@ import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.bearerAuth
-import io.ktor.client.request.contentType
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.http.isSuccess
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.CoroutineScope
